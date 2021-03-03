b1a4b8c35cd5fcf14a3a991c11cfd2043d86f49f
Stephen Lewis <code@stephenlewis.me>
2021-03-03 07:09:20 +0000

    Add craft alias

--- .aliases.d/php.sh
+++ .aliases.d/php.sh
@@ -2,5 +2,6 @@
 # PHP aliases
 # -----------------------------------------------------------------------------
 alias art='php artisan'
+alias craft='php craft'
 alias t='phpunit'
 alias tf='phpunit --filter'
