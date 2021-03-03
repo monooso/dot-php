e8031e464b0688ab4016a0a4a9c177b9f01f20f9
Stephen Lewis <code@stephenlewis.me>
2021-03-03 07:08:42 +0000

    Update PHP paths

--- .zshrc.d/php.zsh
+++ .zshrc.d/php.zsh
@@ -1,6 +1,5 @@
 # ------------------------------------------------
 # PHP
 # ------------------------------------------------
-export PATH="./vendor/bin:$HOME/.composer/vendor/bin:$PATH"
-export PATH="/usr/local/opt/php@7.3/bin:$PATH"
-export PATH="/usr/local/opt/php@7.3/sbin:$PATH"
+export PATH="./vendor/bin:./tools/vendor/bin:$HOME/.composer/vendor/bin:$PATH"
+export PATH="/usr/local/bin/php:$PATH"
